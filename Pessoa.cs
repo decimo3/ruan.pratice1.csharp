798ad2ff928bce1fa6b3210795bd651a9940272f
Ruan Faustino Camello da Silva <ruanfaustinocamellodasilva@gmail.com>
2021-07-20 15:41:05 -0300

    UP: Subindo projeto

---
+++ Pessoa.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Pessoa
+{
+    class chamarNome
+    {
+        private string nomePessoa;
+        private string userName;
+        private string password;
+        private long nascimento;
+        
+
+        public chamarNome(string nome)
+        {
+            Console.WriteLine("Hello {0}", nome);
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Pessoa;
+
+namespace ruan.console.csharp
+{
+    class praticando
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            chamarNome saudar = new chamarNome("Ruan");
+        }
+    }
+}
